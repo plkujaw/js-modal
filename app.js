12be46cbfe236e0d3d5da6f52e5fc53c2b2d4870
plkujaw <jawor.jakub@gmail.com>
2020-06-30 16:15:34 +0100

    modal done

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 // select modal-btn,modal-overlay,close-btn
+
+const modalBtn = document.querySelector(".modal-btn");
+const modalOverlay = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
 // listen for click events on modal-btn and close-btn
-// when user clicks modal-btn add .open-modal to modal-overlay
-// when user clicks close-btn remove .open-modal from modal-overlay
+
+modalBtn.addEventListener("click", () => {
+  // when user clicks modal-btn add .open-modal to modal-overlay
+  modalOverlay.classList.add("open-modal");
+});
+
+closeBtn.addEventListener("click", () => {
+  // when user clicks close-btn remove .open-modal from modal-overlay
+  modalOverlay.classList.remove("open-modal");
+});
+
+// press escape to close the overlay
+document.addEventListener("keydown", (event) => {
+  if(event.keyCode === 27) {
+    modalOverlay.classList.remove("open-modal");
+  }
+});
